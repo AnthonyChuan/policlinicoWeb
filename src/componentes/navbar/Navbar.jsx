ed548a3226f2dddc3fcd42d31c2ed1dcb1b36905
Anthony Chuan <anthonychuan1@gmail.com>
2025-01-07 20:49:03 -0500

    header2

--- src/componentes/navbar/Navbar.jsx
+++ src/componentes/navbar/Navbar.jsx
@@ -2,12 +2,13 @@ import "./navbar.css"
 
 const Navbar = () =>{
     return <header> 
-        <div>
-            <div><img src="../img/logo.png" alt="logo" className="logo"/>
-            <span>POLICLINICO ESPECIALIZADO SAN GABRIEL</span>
+        <div className="contenedor">
+            <div className="logo-clase">
+                <img src="../img/logo.png" alt="logo" className="logo"/>
+                <span>POLICLINICO ESPECIALIZADO SAN GABRIEL</span>
             </div>
         <nav>   
-            <div className="nav__links">
+            <div className="nav-links">
                 <div><a href="#">NOSOTROS</a></div>
                 <div><a href="#">CONTACTO</a></div>
                 <div>ESPECIALIDADES</div>
